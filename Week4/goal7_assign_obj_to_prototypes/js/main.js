8e689528dfd9351e8b00d727cc16fbd199066633
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-08-30 17:49:55 -0400

    final project
    still working final project

---
+++ Week4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ */
+ 
+ /* 
+ 
+ 1)  create array with five names minimum.  These names will be randomly selected for output display. 
+ ****var names = [Jose, Jim, Jack, Jill, Janet]****
+ 
+ 2)  create syntax to randomly pull name from array.
+ *****var namesR = names [Math.floor((Math.random(names) * names.length))];****
+ 
+ 
+ 
+ */
+ 
+ 
+
