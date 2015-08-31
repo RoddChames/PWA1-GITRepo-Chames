6eb6ceb51f19b312465a9c3925855a659c2a3e76
RoddChames <Webmaster@Roddrecuss-MacBook-Pro.local>
2015-08-30 20:54:51 -0400

    final assignment
    finishing assignment.

--- Week4/goal7_assign_obj_to_prototypes/js/main.js
+++ Week4/goal7_assign_obj_to_prototypes/js/main.js
@@ -6,12 +6,26 @@
  
  /* 
  
- 1)  create array with five names minimum.  These names will be randomly selected for output display. 
+ ---------- main.js ----------
+ 
+ 1.)  create array with five names minimum.  These names will be randomly selected for output display. 
  ****var names = [Jose, Jim, Jack, Jill, Janet]****
+
+ 2.)  create syntax to randomly pull name from array using Math.random
  
- 2)  create syntax to randomly pull name from array.
- *****var namesR = names [Math.floor((Math.random(names) * names.length))];****
+ 3.) create "for loop" which will loop three times basically giving the three people their name/job/action and placing it in the iterated row. 
  
+ 4.) if an name is chosen use the pop method to delete it from the array, this will prevent the name from being duplicated.
+ 
+ 5.) create a variable called person and this will contain the instantiating person object. Pass the random name and row into the parameters.
+ 
+ 6.) create a function "populateHTML" which will put the random information provided for the loop onto the screen using innerHTML property.
+ 
+ 7.) set interval to call the following function 30 times per second
+ 
+ 8.) set up the runUpdate function which contains instances of the people array. 
+ 
+ 9.) each instance will be updated 30 times per second.
  
  
  */
--- Week4/goal7_assign_obj_to_prototypes/js/person.js
+++ Week4/goal7_assign_obj_to_prototypes/js/person.js
@@ -4,4 +4,18 @@
  * Goal: Goal7
  */
  
+/* 
+---------- person.js ----------
+ 
+ 1.) make the info in the people.js file available to the main.js file by using the window object.
+ 
+ 2.) create an array that is attached to the person object this array should contain jobs. (ex. person.job = [contractor, doctor, lawyer] )
+ 
+ 3.) create an array that is attached to the person object this array should contain actions a person could perform. (ex. person.job = [driving, working, sleeping] )
+ 
+ 4.) create syntax that picks the rows and columns for my information.
+ 
+ 5.) create an update function and pass the information for the action column into the parameters.  This will update every five seconds.
+ 
+ */
  
